67fad946a1fa5005ca8126f5377c1bc36835fdef
Huaxu <yhxchem@outlook.com>
2024-09-03 11:25:22 -0700

    modified:   README.md 	modified:   pyproject.toml 	new file:   src/masscube/__pycache__/msjson.cpython-311.pyc 	modified:   src/masscube/__pycache__/workflows.cpython-311.pyc 	modified:   src/masscube/msjson.py

--- README.md
+++ README.md
@@ -1,39 +1,37 @@
 # MassCube
 
-[![Generic badge](https://img.shields.io/badge/masscube-ver_0.0.16-%3CCOLOR%3E.svg)](https://github.com/huaxuyu/masscube/)
+[![Generic badge](https://img.shields.io/badge/masscube-ver_1.0.17-%3CCOLOR%3E.svg)](https://github.com/huaxuyu/masscube/)
 ![Maintainer](https://img.shields.io/badge/maintainer-Huaxu_Yu-blue)
 [![PyPI Downloads](https://img.shields.io/pypi/dm/bago.svg?label=PyPI%20downloads)](https://pypi.org/project/masscube/)
 
 **masscube** is an integrated Python package for liquid chromatography-mass spectrometry (LC-MS) data processing.
 
-* **Documentation:** https://masscube.readthedocs.io/en/latest/
-* **Source code:** https://github.com/huaxuyu/masscube/
-* **Bug reports:** https://github.com/huaxuyu/masscube/issues/
+- **Documentation:** https://huaxuyu.github.io/masscubedocs/
+- **Source code:** https://github.com/huaxuyu/masscube/
+- **Bug reports:** https://github.com/huaxuyu/masscube/issues/
 
 It provides:
 
-* Ion-identity-informed chromatograpgic peak picking.
-* Peak quality evaluation via artificial neural network.
-* Accurate annotation of isotopes, adducts, and in-source fragments.
-* Ultra-fast annotation of MS/MS spectra supported by [Flash Entropy Search](https://github.com/YuanyueLi/MSEntropy)
+- High performance feature detection based on signal clustering.
+- Comprehensive feature quality evaluation.
+- Confident annotation of isotopes, adducts, and in-source fragments.
+- Ultra-fast annotation of MS/MS spectra supported by [Flash Entropy Search](https://github.com/YuanyueLi/MSEntropy)
 
 ## Installation
 
-```sh
-# PyPI
+```bash
 pip install masscube
 ```
 
-The changes to **masscube** between each release can be found [here](https://pypi.org/project/masscube/#history). See more from the commit logs.
+To upgrade **masscube** to the latest version:
+
+```bash
+pip install masscube --upgrade
+```
 
 ## Quick start
 
-Over 100 fundemental functions and objects are available in **masscube** to help you create the best data processing workflow for your study. Some examples of the pre-made workflows are here:
-
-* Untargeted metabolomics workflow
-* Data quality examination
-* MS/MS search (identity search and hybrid search)
-* Visualize MS data and generate plots ready for publication
+Start your untargeted metabolomics data processing from [here](https://huaxuyu.github.io/masscubedocs/docs/quickstart/)
 
 ## Contribute to masscube
 
@@ -41,4 +39,4 @@ The **masscube** project is excited to have your expertise and passion on board!
 
 We value all enhancements or corrections. For those thinking about making significant contributions to the codebase, we encourage you to get in touch with us!
 
-* Huaxu Yu, hxuyu@ucdavis.edu
+- Huaxu Yu, hxuyu@ucdavis.edu
--- src/masscube/msjson.py
+++ src/masscube/msjson.py
@@ -18,12 +18,13 @@ msjson design:
 
 import json
 import os
+import gzip
 
 from . import read_raw_file_to_obj
 from .params import find_ms_info
 
 def rawdata_to_msjson(raw_data_file: str, msjson_file: str = None, int_tol_ms1: float = None,
-                      int_tol_ms2: float = None):
+                      int_tol_ms2: float = None, compression: bool = True):
     
     """
     Convert the MSData object to msjson format.
@@ -42,8 +43,8 @@ def rawdata_to_msjson(raw_data_file: str, msjson_file: str = None, int_tol_ms1: 
         print(f"Error: {raw_data_file} does not exist.")
         return None
     
-    if not msjson_file.lower().endswith(".mzml") or not msjson_file.lower().endswith(".mzxml"):
-        print(f"Error: {msjson_file} should be a mzML or mzXML file.")
+    if not raw_data_file.lower().endswith(".mzml") and not raw_data_file.lower().endswith(".mzxml"):
+        print(f"Error: {raw_data_file} should be a mzML or mzXML file.")
         return None
 
     # initialize the msjson file
@@ -63,7 +64,51 @@ def rawdata_to_msjson(raw_data_file: str, msjson_file: str = None, int_tol_ms1: 
     # find the metadata
     ms_type, ion_mode, centroid = find_ms_info(raw_data_file)
 
-    if ms_type == "tof":
-        int_tol_ms1 = 1000
+    if not centroid:
+        print(f"Error: {raw_data_file} is not centroided.")
+        return None
 
-    d = read_raw_file_to_obj(raw_data_file)
+    if ms_type == "tof":
+        int_tol_ms1 = 500
+    elif ms_type == "orbitrap":
+        int_tol_ms1 = 10000
+
+    d = read_raw_file_to_obj(raw_data_file, int_tol = int_tol_ms1)
+
+    msjson["metadata"]["name"] = d.file_name
+    msjson["metadata"]["ion_mode"] = ion_mode
+    msjson["metadata"]["rt_start"] = d.ms1_rt_seq[0]
+    msjson["metadata"]["rt_end"] = d.ms1_rt_seq[-1]
+    msjson["metadata"]["instrument_type"] = ms_type
+    
+    for i in d.scans:
+        if i.level == 1:
+            msjson["scans"].append({
+                "level": i.level,
+                "time": i.rt,
+                "mz": list(i.mz_seq),
+                "intensity": list(i.int_seq),
+            })
+        if i.level == 2:
+            msjson["scans"].append({
+                "level": i.level,
+                "time": i.rt,
+                "mz": list(i.peaks[:,0]),
+                "intensity": list(i.peaks[:,1]),
+                "precursor_mz": round(i.precursor_mz, 4)
+            })
+    for i in msjson["scans"]:
+        i["mz"] = [round(x, 4) for x in i["mz"]]
+        i["intensity"] = [f"{x:.3e}" for x in i["intensity"]]
+
+    # compress the msjson file
+    if compression:
+        if msjson_file is None:
+            msjson_file = os.path.join(os.path.dirname(raw_data_file), os.path.basename(raw_data_file).split(".")[0] + ".msjson.gz")
+        with gzip.open(msjson_file, "wt") as f:
+            json.dump(msjson, f)
+    else:
+        if msjson_file is None:
+            msjson_file = os.path.join(os.path.dirname(raw_data_file), os.path.basename(raw_data_file).split(".")[0] + ".msjson")
+        with open(msjson_file, "w") as f:
+            json.dump(msjson, f)
